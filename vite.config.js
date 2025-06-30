fd45af092dedfa431b798e8ce7a51050507f5df3
Matthew Turner <43834067+turnerturn@users.noreply.github.com>
2025-06-30 20:37:08 +0000

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/work-order-portal/', // required for GitHub Pages deployment
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
